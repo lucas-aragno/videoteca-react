fcec807aa48fa18419243e408fd124356346b01e
Lucas Aragno <lucas.aragno@moove-it.com>
2016-02-08 00:44:03 -0300

    Add styles for posters

--- src/javascript/components/MoviePoster.js
+++ src/javascript/components/MoviePoster.js
@@ -1,12 +1,12 @@
 import React, { Component, PropTypes } from 'react'
 
-
+import '../../stylesheets/poster.css'
 
 export default class MoviePoster extends Component {
 
   static propTypes = {
     poster: PropTypes.string,
-    alt: PropTypes.string,
+    movieId: PropTypes.number,
     title: PropTypes.string
   }
 
@@ -17,23 +17,11 @@ export default class MoviePoster extends Component {
   }
 
   render() {
+    const { title } = this.props
     return (
-      <div className="poster-margin">
-        <div className="poster poster-skyfall">
-          <div className="poster-skyfall-lyr3"></div>
-          <div className="poster-skyfall-lyr2"></div>
-          <div className="poster-skyfall-lyr1"></div>
-          <div className="bg poster-skyfall-bg">
-            <div className="poster-skyfall-lyr5"></div>
-            <div className="glow"></div>
-          </div>
-          <div className="bg poster-skyfall-bg bg-2">
-            <div className="poster-skyfall-lyr4"></div>
-          </div>
-          <div className="shadow"></div>
-        </div>
-        <div className="poster-title">Skyfall</div>
-    </div>
+      <div className="movie-poster" style={this.divStyle()}>
+        <span> { title } </span>
+      </div>
     )
   }
 }
